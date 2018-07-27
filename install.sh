e62a93eca9ec985375affa8865203604e6baf6ab
Tim Wanders <timwanders241@gmail.com>
2018-07-27 20:55:47 +0200

    [install.sh] prepare vim

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
-git submodule update 
+git submodule update --init
+
 dir="$(pwd)"
 for config_dir in bspwm sxhkd gtk-3.0
 do
@@ -9,9 +10,11 @@ do
         "$HOME/.config/$config_dir"
 done
 
-rm -rf $HOME/.vim
+rm -rf "$HOME/.vim"
 ln -sf "$dir/.vim"       "$HOME/.vim"
 
 ln -sf "$dir/.vimrc"     "$HOME/.vimrc"
 ln -sf "$dir/.gtkrc-2.0" "$HOME/.gtkrc-2.0"
+
+yes | vim -c :PluginInstall -c :qa!
 
