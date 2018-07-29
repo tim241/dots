38c04673c71ca392a2cd4f43c32cdd5e5b6ae9b6
Tim Wanders <timwanders241@gmail.com>
2018-07-29 17:31:47 +0200

    [pacman.sh] added dmenu

--- .install/pacman.sh
+++ .install/pacman.sh
@@ -2,7 +2,7 @@
 set -e
 for package in bspwm sxhkd rxvt-unicode \
                 xorg-server xorg-xsetroot xorg-xinit \
-                redshift pulseaudio firefox neovim
+                redshift pulseaudio firefox neovim dmenu
 do
     if ! pacman -Qi $package > /dev/null 2>&1
     then
