773af370c5ff0c6632132bfeb97a77adcb1a4df3
Tim Wanders <tim241@mailbox.org>
2019-01-09 01:00:27 +0100

    removed discord theme
    I don't use discord anymore, IRC ftw

--- install.sh
+++ install.sh
@@ -91,12 +91,3 @@ do
     fi
 done
 
-# Installing discord theme
-for dir in "$HOME/.config"/discord/*.*.*/modules/discord_desktop_core
-do
-    if [ ! -f "$dir/discord-custom.css" ] && \
-        [ -d "$dir" ]
-    then
-        ln -sf "$(pwd)/custom/discord/discord-custom.css" "$dir/discord-custom.css"
-    fi
-done
