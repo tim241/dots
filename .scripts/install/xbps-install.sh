273141d8b3613bb14d28af66a909799170e8f6f2
Tim Wanders <tim241@mailbox.org>
2019-02-10 17:13:41 +0000

    [install] Added xbps-install support

---
+++ .scripts/install/xbps-install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+for package in bspwm sxhkd rxvt-unicode \
+                xorg xinit xsetroot feh \
+                pulseaudio \
+                redshift firefox weechat \
+                neovim rofi dunst \
+                font-fira-otf \
+                xdg-utils xdg-user-dirs \
+                bash-completion \
+                autocutsel \
+                w3m neomutt offlineimap \
+                acpi \
+                scrot xclip unclutter pulsemixer \
+                compton \
+                polybar
+do
+    if ! xbps-query "$package" &> /dev/null
+    then
+        install_pkgs+=("$package")
+    fi
+done
+if [ "$install_pkgs" ]
+then
+    sudo xbps-install -S --yes "${install_pkgs[@]}"
+fi
--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ set -e
 [ ! -d "$HOME/.config" ] && mkdir "$HOME/.config"
 [ ! -d "$HOME/.themes" ] && mkdir "$HOME/.themes"
 
-for pkg_man in pacman apt-get
+for pkg_man in pacman apt-get xbps-install
 do
     if command -v "$pkg_man" > /dev/null 2>&1
     then
