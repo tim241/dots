1ca667287c0365afea2b4d7a73ee0e141564fbb8
Tim Wanders <tim241@mailbox.org>
2019-02-14 18:06:33 +0100

    [themes] Add void linux wallpaper

--- .config/tim241/themes/gruvbox-soft/wallpaper.sh
+++ .config/tim241/themes/gruvbox-soft/wallpaper.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
+function set_wall()
+{
+    feh --bg-fill "$@"
+    exit
+}
+
 config_dir="${XDG_CONFIG_HOME:-$HOME/.config}"
+wall_dir="$config_dir/tim241/themes/gruvbox-soft/walls/"
+
 xsetroot -solid '#32302f'
-feh --bg-fill "$config_dir/tim241/themes/gruvbox-soft/walls/wall1.png"
+
+if [ -f "/bedrock/etc/os-release" ]
+then
+    set_wall "$wall_dir/brl.png"
+fi
+
+source /etc/os-release
+
+if [ "$NAME" = "void" ]
+then
+    set_wall "$wall_dir/void.png"
+fi
