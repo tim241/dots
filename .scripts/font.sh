c253cf035c995df2557e5cdca6f10541f6431088
Tim Wanders <tim241@mailbox.org>
2019-04-05 12:58:12 +0200

    [install] Fixed font script

--- .scripts/font.sh
+++ .scripts/font.sh
@@ -2,7 +2,7 @@
 set -e
 font_dir="$HOME/.local/share/fonts"
 
-if [ -d "$font_dir/fira" ]
+if [ -d "$font_dir/Fira" ]
 then
     exit
 fi
@@ -12,4 +12,6 @@ then
     mkdir -p "$font_dir"
 fi
 
-git clone https://github.com/mozilla/Fira "$font_dir" --depth=1
+git clone https://github.com/mozilla/Fira "$font_dir/Fira" --depth=1
+
+fc-cache -f
