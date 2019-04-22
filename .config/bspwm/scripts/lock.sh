07c066f5a86090e03203b82dc5c0b91a7afee1a5
Tim Wanders <tim241@mailbox.org>
2019-04-22 22:19:47 +0200

    [bspwm] pause mpd on lock

--- .config/bspwm/scripts/lock.sh
+++ .config/bspwm/scripts/lock.sh
@@ -7,6 +7,12 @@ fi
 
 config_dir="${XDG_CONFIG_HOME:-$HOME/.config}/bspwm"
 
+mpc_continue=no
+if mpc pause-if-playing &> /dev/null
+then
+    mpc_continue=yes
+fi
+
 if [ "$(man i3lock | grep i3lock-color)" ]
 then
     "$config_dir/bin/betterlockscreen" -l > /dev/null
@@ -14,3 +20,7 @@ else
     i3lock -c '#32302f'
 fi
 
+if [ "$mpc_continue" = "yes" ]
+then
+    mpc play
+fi
